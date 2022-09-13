afd932fadd0dcdc77b57dfdc033664b4c1c5027c
OzerMV <M.Ozerov@bssys.com>
2022-09-13 19:31:35 +0300

    BSSAURAF-22 pull before work

--- collect.bat
+++ collect.bat
@@ -1,4 +1,5 @@
 :: %1 - token
 :: %2 - gitlab url
 
+docker pull aurabss/aurasdk
 docker run -it --rm --env=AURA_GIT_URL=%2 --env AURA_GIT_TOKEN=%1 --name aurasdk -v %cd%:/out -v %cd%:/cache aurabss/aurasdk
