440ef393832b5778783490dd666e0f0c3556e076
OzerMV <M.Ozerov@bssys.com>
2022-09-14 15:13:18 +0300

    BSSAURAF-22 simultaneous work on several repos

--- collect.bat
+++ collect.bat
@@ -2,4 +2,4 @@
 :: %2 - gitlab url
 
 docker pull aurabss/aurasdk
-docker run -it --rm --env=AURA_GIT_URL=%2 --env AURA_GIT_TOKEN=%1 --name aurasdk -v %cd%:/out -v %cd%:/cache aurabss/aurasdk
+docker run -it --rm --env=AURA_GIT_URL=%2 --env AURA_GIT_TOKEN=%1 -v %cd%:/out -v %cd%:/cache aurabss/aurasdk
